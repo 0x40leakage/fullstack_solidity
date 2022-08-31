9a04f186c1e3f1b88a944758bee0a17a75063136
hofungkoeng <zoeyeooz@gmail.com>
2022-08-31 17:50:56 +0800

    etherscan api key

--- hardhat_fund_me/hardhat.config.js
+++ hardhat_fund_me/hardhat.config.js
@@ -32,4 +32,7 @@ module.exports = {
             1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
     },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 }
